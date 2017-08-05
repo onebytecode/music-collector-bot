46e9bb814f8e3ab9d08eedddf762d40525ac24e6
CashALot <goncharovnikita.dev@gmail.com>
2017-08-05 19:03:01 +0300

    tests

--- modules/request_builder/wiki.js
+++ modules/request_builder/wiki.js
@@ -1,10 +1,11 @@
 // WIKI REQUEST MODULE
 
+const { normalize } =  require('./helpers')
 module.exports = () => {
   const ruWikiEndpoint = 'https://ru.wikipedia.org/w/api.php'
   const getPage = (n) => {
     if (typeof n !== 'string' || !n) throw new Error('Name should be a string')
-    const name = n
+    const name = normalize({ type: 'name', el: n })
     const action = "?action=query"
     const titles = "titles=" + name.trim()
     const format = "format=json"
